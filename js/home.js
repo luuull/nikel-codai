e92b95667f2a899d826a3c2eb152b9f48f5af55e
Luana Souza <101467080+luuull@users.noreply.github.com>
2022-03-25 02:02:35 -0300

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,5 +1,4 @@
 const myModal = new bootstrap.Modal("#transaction-modal");
-
 let logged = sessionStorage.getItem("logged");
 const session = localStorage.getItem("session");
 
@@ -9,8 +8,8 @@ let data = {
 
 document.getElementById("button-logout").addEventListener("click", logout);
 document.getElementById("button-transactions").addEventListener("click", function() {
-    window.location.href = "transactions.html"
-});
+    window.location.href = "transactions.html";
+})
 
 
 //adicionar lançamento
@@ -38,7 +37,7 @@ document.getElementById("transaction-form").addEventListener("submit", function(
     getTotal();
 
     alert("Lançamento adicionado com sucesso.");
-});
+})
 
 checkLogin();
 
@@ -79,15 +78,15 @@ function getCashIn() {
         }
         for (let index = 0; index < limit; index++) {
             cashInHtml += `  
-            <div class="row mb-4 ">
-                <div class="col-12 ">
+            <div class="row mb-4">
+                <div class="col-12">
                 <h3 class=" fs-2 ">R$ ${cashIn[index].value.toFixed(2)}</h3>
-                <div class="container p-0 ">
-                    <div class="row ">
-                        <div class="col-12 col-md-8 ">
+                <div class="container p-0">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
                             <p>${cashIn[index].description}/</p>
                         </div>
-                        <div class="col-12 col-md-3 d-flex justify-content-end ">
+                        <div class="col-12 col-md-3 d-flex justify-content-end">
                             ${cashIn[index].date}
                         </div>
                     </div>
@@ -97,7 +96,7 @@ function getCashIn() {
             `
         }
 
-        document.getElementById("cash-in-list").innerHTML = cashInHtml;
+        document.getElementById("cashInList").innerHTML = cashInHtml;
 
     }
 
@@ -116,18 +115,17 @@ function getCashOut() {
         } else {
             limit = cashIn.length;
         }
-        
         for (let index = 0; index < limit; index++) {
             cashInHtml += `  
-            <div class="row mb-4 ">
-                <div class="col-12 ">
+            <div class="row mb-4">
+                <div class="col-12">
                 <h3 class=" fs-2 ">R$ ${cashIn[index].value.toFixed(2)}</h3>
-                <div class="container p-0 ">
-                    <div class="row ">
-                        <div class="col-12 col-md-8 ">
+                <div class="container p-0">
+                    <div class="row">
+                        <div class="col-12 col-md-8">
                             <p>${cashIn[index].description}/</p>
                         </div>
-                        <div class="col-12 col-md-3 d-flex justify-content-end ">
+                        <div class="col-12 col-md-3 d-flex justify-content-end">
                             ${cashIn[index].date}
                         </div>
                     </div>
@@ -137,7 +135,7 @@ function getCashOut() {
             `
         }
 
-        document.getElementById("cash-out-list").innerHTML = cashInHtml;
+        document.getElementById("cashOutList").innerHTML = cashInHtml;
 
     }
 
